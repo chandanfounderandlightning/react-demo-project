65c31e5ab3c2fb88e2ed47adbc410965efd1163c
ucreateuser <chandankumar@ucreate.co.in>
2018-04-18 15:35:21 +0530

    Design improvement of faq page

--- src/components/Faqlist/Faqlistcontent.js
+++ src/components/Faqlist/Faqlistcontent.js
@@ -5,15 +5,15 @@ export default class Faqlistcontent extends React.Component {
     render() {
         const faqQues = this.props.data.map((item) =>
             <div className="col-xs-12 col-sm-6 m-b-30">
-                        <div className="col_services">
-                            <h3>{item.label}</h3>
-                            <p>{item.value}</p>
-                        </div>
-                    </div>
+                <div className="col_services">
+                    <h3>{item.label}</h3>
+                    <p>{item.value}</p>
+                </div>
+            </div>
           );
         return(
             <div className="faq_row">
-	             {faqQues}
+	           {faqQues}
             </div>
         )
     }
